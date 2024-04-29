204fb215bf01030e27e34c1cbc36d4d746112dda
Ridan Hawi <ridanhawy@gmail.com>
2024-04-29 22:59:43 +0300

    trying changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 import urllib.request
-
 """
-Script that fetches the status of the Alx Intranet website and prints information about the response.
-
-This script utilizes the urllib.request module to make an HTTP GET request to the URL https://alx-intranet.hbtn.io/status.
-It then retrieves the response body and prints information about it, including the type of the response body, the raw content,
-and the decoded content assuming it is encoded in UTF-8.
+Script that fetches https://intranet.hbtn.io/status
 """
+
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode(encoding="utf-8")))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reply:
+        body = reply.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
+    
